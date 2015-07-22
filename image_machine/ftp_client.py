16ee851ec2b968b6038cc957c91910a8ff1eec29
Hector Rios <labeledloser@gmail.com>
2015-07-22 10:21:27 -0500

    Added classes to interface with the FTP.
    - Created the initial application (which is
      `image_machine/image_machine.py`).
    - Wired up the `ImageCsv` class to download the images, update the file
      with the correct relative image path.
    - Cleaned up unnecessary methods.

--- image_machine/ftp_client.py
+++ image_machine/ftp_client.py
@@ -1,44 +1,177 @@
+import settings
+import util
 from ftplib import FTP
 from os import path
-from settings import Settings
 
 class FtpClient(object):
-    def __init__(self):
-        settings = Settings()
-        self.host = settings.get_ftp_value('Host')
-        self.port = settings.get_ftp_value('Port')
-        self.username = settings.get_ftp_value('Username')
-        self.password = settings.get_ftp_value('Password')
+    """
+    The FTP client is an interface to talk to our FTP using the credentials
+    specified in the configuration file. It allows us to Create, Delete,
+    Download files on the FTP.
+    """
+    def __init__(self, config_file=None):
+        settings.init(config_file)
+        self.host = settings.get_setting('ftp').get('host')
+        self.username = settings.get_setting('ftp').get('username')
+        self.password = settings.get_setting('ftp').get('password')
         self.connection = None
 
     def __enter__(self):
-        if not self.connection:
-            self.open_ftp()
+        """
+        Allows us to use the `with` statement. It opens up an FTP connection.
+        """
+        self.open_ftp()
+        return self
 
-    def __exit__(self, exception, exception_value, tracebook):
+    def __exit__(self):
+        """
+        Safely closes the ftp connection when used in conjuction with `with`.
+        """
         self.close_ftp()
 
     def open_ftp(self):
+        """
+        Returns the active connection is one is available. If not, it creates a
+        new connection and returns that.
+        """
         if not self.connection:
-            self.connection = FTP(self.host, self.port)
+            self.connection = FTP(self.host)
             self.connection.login(self.username, self.password)
-            print('Connected to {0}:{1}.'.format(self.host, self.port))
+            util.conditional_print('Connected to {0}.'.format(self.host))
 
         return self.connection
 
     def close_ftp(self):
+        """
+        If a connection exists, it closes it. To avoid having to frequently
+        call this, consider using the `with` statement.
+        """
         if self.connection:
             self.connection.quit()
 
-        print('Connection to {0} closed.'.format(self.host))
+        util.conditional_print('Connection to {0} closed.'.format(self.host))
+
+    def get_directory_contents(self, directory_path=''):
+        """
+        Returns the FTP directory contents.
+        """
+        files = []
+        self.connection.dir(directory_path, files.append)
+
+        return files
+
+    def list_ftp_files(self, directory_path=''):
+        """
+        Returns the FTP directory files.
+        """
+        import re
+        whitespace_re = re.compile(r'\s+')
+        files = self.get_directory_contents(directory_path)
+        formatted_files = []
+
+        for f in files:
+            result = whitespace_re.sub(' ', f).split(' ')
+            if result[-2].isdigit():
+                formatted_files.append(result[-1])
+
+        return formatted_files
+
+    def list_ftp_directories(self, directory_path=''):
+        """
+        Returns the FTP directory folders.
+        """
+        import re
+        whitespace_re = re.compile(r'\s+')
+        files = self.get_directory_contents(directory_path)
+
+        formatted_files = []
+
+        for f in files:
+            result = whitespace_re.sub(' ', f).split(' ')
+            if result[-2] == '<DIR>':
+                formatted_files.append(result[-1])
+
+        return formatted_files
 
     def get_file(self, filename, destination):
+        """
+        Retrieves a single file from the FTP.
+        """
         local_filename = path.join(destination, filename)
 
         with open(local_filename, 'wb') as local_file:
-            self.connection.retbinary('RETR {0}'.format(filename), local_file.write, 8*1024)
+            self.connection.retrbinary('RETR {0}'.format(filename), local_file.write, 8*1024)
 
-    def get_files(self, *filenames):
+    def get_files(self, filenames):
+        """
+        Retrieves the given filenames from the FTP.
+        """
         for filename in filenames:
+            util.conditional_print("Getting File: {0}".format(filename))
             self.get_file(filename, '.')
+
+    def delete_files(self, files):
+        """
+        Removes the given filenames from the FTP.
+        """
+        for f in files:
+            util.conditional_print("About to delete file: {0}".format(f))
+            try:
+                self.connection.delete(f)
+            except e:
+                print("Unable to delete file: {0}\nError: {1}".format(f, e))
+
+    def get_all_files(self):
+        """
+        Lists all the directory contents, and retrieves the files that we can
+        use.
+        """
+        files = self.select_allowed_files(self.list_ftp_files())
+
+        util.work_in_tmp_directory('tmp')
+
+        self.get_files(files)
+
+        return files
+
+    def select_allowed_files(self, files):
+        """
+        Defines what files we are able to use.
+        """
+        allowed_file_extensions = ['csv', 'txt']
+
+        return [f for f in files if f.split('.')[-1] in allowed_file_extensions]
+
+    def is_ftp_directory(self, dir_name, base_directory_path=''):
+        """
+        Checks whether or not the given `dir_name` is a directory within the
+        `base_directory_path` location.
+        """
+        directories = list_ftp_directories(base_directory_path)
+        return directories.count(dir_name) > 0
+
+    def make_ftp_directory(self, dir_name, base_directory_path=''):
+        """
+        Safely creates an FTP directory if one does not exist.
+        """
+        if not is_ftp_directory(dir_name, base_directory_path):
+            self.connection.mkd(dir_name)
+
+    def upload_files(self, files, destination='done'):
+        """
+        Uploads a list of files to the given destination.
+        """
+        if destination:
+            self.make_ftp_directory(destination)
+            self.connection.cwd(destination)
+
+        for f in files:
+            self.upload_file(f)
+
+    def upload_file(self, the_file):
+        """
+        Uploads a single file to the FTP by transferring binary.
+        """
+        with open(the_file, 'rb') as f:
+            self.connection.storbinary('STOR {0}'.format(the_file), f)
 
---
+++ image_machine/image_machine.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+from ftp_client import FtpClient
+from image_csv import ImageCsv
+import util
+
+def main():
+    """
+    The main program execution flow. It provides the user with command-line
+    arguments. Also, it downloads the files from the FTP and performs the
+    processing on each file.
+    """
+    options = command_line_parser()
+
+    if not options.config_file:
+        print("No configuration file used. Defaulting to config.json")
+        options.config_file = 'image_machine/config.json'
+
+    files = download_files_from_ftp(options.config_file, options.debug)
+    process_files(files, options.config_file, options.debug)
+
+def download_files_from_ftp(configuration, use_debug):
+    """
+    The main worker for downloading the files from the FTP. It attempts to
+    safely download files from the FTP server.
+    """
+    files = []
+    try:
+        with FtpClient(configuration) as ftp_client:
+            files = ftp_client.get_all_files()
+    except e:
+        util.conditional_print(
+                'An error occured while trying to retrieve files via FTP.',
+                'ERROR')
+        util.conditional_print(e, 'ERROR')
+    finally:
+        return files
+
+def process_files(files, configuration, use_debug):
+    """
+    The main worker for processing each file that was downloaded.
+    """
+    for f in files:
+        importer = ImageCsv(f)
+        importer.perform_magic()
+
+def command_line_parser():
+    """
+    Provides command line options. It also provides details as to how to use each flag.
+    """
+    parser = ArgumentParser(
+            description="ಠ_ಠ Batch download files and create an export.")
+    parser.add_argument("-c", "--config", dest="config_file",
+            help="The configuration file that should be used by the application")
+    parser.add_argument("-d", "--debug", dest="debug",
+            help="Want to see debug statements?", action="store_true")
+
+    args = parser.parse_args()
+
+    return args
+
+def quit_program():
+    """
+    The final print statement! Make it count.
+    """
+    print("I hope you got the data you wanted. Rock on. \m/")
+
+if __name__ == "__main__":
+    main()
+    quit_program()
+
